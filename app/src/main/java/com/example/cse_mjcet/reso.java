3d414c18936233054c46988fa643573967f7afa7
salman-am <salman102000@gmail.com>
2019-12-30 20:01:06 +0530

    commit

--- app/src/main/java/com/example/cse_mjcet/reso.java
+++ app/src/main/java/com/example/cse_mjcet/reso.java
@@ -6,118 +6,221 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-
-public class reso extends AppCompatActivity implements View.OnClickListener {
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import java.util.ArrayList;
+public class reso extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reso);
         /*getActionBar().setTitle("Student Resources");*/
-    }
 
-    @Override
-    public void onClick(View v) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("AFREEN SULTANA");
+        arrayList.add("AHMED");
+        arrayList.add("DR A.A MOIZ QYSER");
+        arrayList.add("FAHMINA TARNUM");
+        arrayList.add("J. SRINIVAS");
+        arrayList.add("K. MANMOHAN RAO");
+        arrayList.add("K.S. NIRAJA");
+        arrayList.add("K. SRIDEVI");
+        arrayList.add("KHAJA ZAHOORUDDIN AHMED");
+        arrayList.add("KRISHNA KEERTHI CHANNAM");
+        arrayList.add("MANIZA HIJAB");
+        arrayList.add("MANJUSHA KALEKURI");
+        arrayList.add("MD. SALEEM KHAN");
+        arrayList.add("MD. ZAINUDDIN NAVEED");
+        arrayList.add("MIR AHMED ALI");
+        arrayList.add("MOHAMMED ABDUL RAHEEM");
+        arrayList.add("MOHAMMED AHMED");
+        arrayList.add("MOHAMMED MAHMOOD ALI");
+        arrayList.add("MOHAMMED NAZEER");
+        arrayList.add("MD. SHABAZ HUSSAIN");
+        arrayList.add("MD.SHABAZ HUSSAIN");
+        arrayList.add("MOHAMMED SHARFUDDIN");
+        arrayList.add("MOHAMMED UMAR FAROOQ");
+        arrayList.add("NAIMOONISA BEGUM");
+        arrayList.add("RAJESHAM GAJULA");
+        arrayList.add("SHOUKAT ALI");
+        arrayList.add("SYED MD AKBAR HASMI");
+        arrayList.add("SYED MOHIUDDIN");
+        arrayList.add("SYED SHABBEER AHMAD");
+        arrayList.add("SYED AMBAREEN RANA");
+        arrayList.add("UMA N. DULHARE");
 
-        int id=v.getId();
-        Intent i;
-        i =new Intent(this,WebActivity.class);
-        String s;
-        switch (id)
-        {
-            case R.id.AFREEN : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=afreensultana";
-            i.putExtra("URL",s);
-            startActivity(i);break;
-            case R.id.AHMED:
-                Toast.makeText(this,"NOTHING TO DISPLAY",Toast.LENGTH_LONG).show();break;
-            case R.id.hod:
-                Toast.makeText(this,"NOTHING TO DISPLAY",Toast.LENGTH_LONG).show();break;
-            case R.id.FAHMINA : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=ftaranum";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.SRINIVAS : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=jsrinivas";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.MANMOHAN : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=kmanmohan";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.NIRAJA : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=ksniraja";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.SRIDEVI : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=sridevi";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.zahoOR : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=zahooruddin";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.keerTHI : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=krishna.keerthi";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.manIZA : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=manizahijab";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.manjusha : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=manjusha";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.SALEEM : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=saleemkhan";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.ZAIN : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=zainuddin";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.ahmedALI : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=mir.ahmedali";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.RAHEEM : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=maraheem";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.mAHMED : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=mohammed.ahmed";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.MAHMOOD : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=mahmood";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.NAZEER : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=nazeer";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.SHAHBAZ : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=shabaz";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.SHAHBAZ2 : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=shabaz.hussain";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.sharfu : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=sharfuddin";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.UMAR : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=umarfarooq";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.NAIM : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=naimoonisa";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.RAJESHAM : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=rajesham.gajula";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.SHOUKAT : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=shoukatali";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.AKBAR : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=akbar.hashmi";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.MOHIUDDIN : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=syed";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.SHABBEER : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=shabbeer.ahmad";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.RANA : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=ambareen.rana";
-                i.putExtra("URL",s);
-                startActivity(i);break;
-            case R.id.UMA : s="http://mjcollege.ac.in/studentresourceslist.php?resourceusername=uma.dulhare";
-                i.putExtra("URL",s);
-                startActivity(i);break;
 
-        }
+        ListView listView = findViewById(R.id.mylist);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String identity=arrayAdapter.getItem(position);
+                Intent i;
+                i =new Intent(reso.this,WebActivity.class);
+                String s;
+                switch (identity) {
+                    case "AFREEN SULTANA":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=afreensultana";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "FAHMINA TARNUM":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=ftaranum";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "AHMED":
+                        Toast.makeText(reso.this,"Nothing to display",Toast.LENGTH_SHORT).show();
+                        break;
+                    case "DR A.A MOIZ QYSER":
+                        Toast.makeText(reso.this,"Nothing to display",Toast.LENGTH_SHORT).show();
+                        break;
+                    case "J. SRINIVAS":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=jsrinivas";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "K. MANMOHAN RAO":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=kmanmohan";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "K.S. NIRAJA":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=ksniraja";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "K. SRIDEVI":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=sridevi";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "KHAJA ZAHOORUDDIN AHMED":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=zahooruddin";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "KRISHNA KEERTHI CHANNAM":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=krishna.keerthi";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "MANIZA HIJAB":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=manizahijab";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "MANJUSHA KALEKURI":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=manjusha";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "MD. SALEEM KHAN":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=saleemkhan";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "MD. ZAINUDDIN NAVEED":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=zainuddin";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "MIR AHMED ALI":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=mir.ahmedali";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "MOHAMMED ABDUL RAHEEM":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=maraheem";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "MOHAMMED AHMED":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=mohammed.ahmed";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "MOHAMMED MAHMOOD ALI":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=mahmood";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "MOHAMMED NAZEER":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=nazeer";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "MD. SHABAZ HUSSAIN":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=shabaz";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "MD.SHABAZ HUSSAIN":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=shabaz.hussain";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "MOHAMMED SHARFUDDIN":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=sharfuddin";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "MOHAMMED UMAR FAROOQ":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=umarfarooq";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "NAIMOONISA BEGUM":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=naimoonisa";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "RAJESHAM GAJULA":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=rajesham.gajula";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "SHOUKAT ALI":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=shoukatali";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "SYED MD AKBAR HASMI":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=akbar.hashmi";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "SYED MOHIUDDIN":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=syed";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "SYED SHABBEER AHMAD":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=shabbeer.ahmad";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "SYED AMBAREEN RANA":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=ambareen.rana";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
+                    case "UMA N. DULHARE":
+                        s = "http://mjcollege.ac.in/studentresourceslist.php?resourceusername=uma.dulhare";
+                        i.putExtra("URL", s);
+                        startActivity(i);
+                        break;
 
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cse_mjcet/youtube.java
+++ app/src/main/java/com/example/cse_mjcet/youtube.java
@@ -5,67 +5,112 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
-public class youtube extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+
+public class youtube extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube);
         /*getActionBar().setTitle("Student Resources");*/
-    }
 
-    @Override
-    public void onClick(View v) {
 
-        int id = v.getId();
-        Intent i;
-        i = new Intent(this, WebActivity.class);
-        String s;
-        switch (id) {
-            case R.id.Clang:
-                s = "https://www.youtube.com/playlist?list=PLBlnK6fEyqRggZZgYpPMUxdY1CYkZtARR";
-                i.putExtra("URL", s);
-                startActivity(i);
-                break;
-            case R.id.Cplus:
-                s = "https://www.youtube.com/playlist?list=PLfVsf4Bjg79Cu5MYkyJ-u4SyQmMhFeC1C";
-                i.putExtra("URL", s);
-                startActivity(i);
-                break;
-            case R.id.DS:
-                s = "https://www.youtube.com/playlist?list=PL2_aWCzGMAwI3W_JlcBbtYTwiQSsOTa6P";
-                i.putExtra("URL", s);
-                startActivity(i);
-                break;
-            case R.id.JAVA:
-                s = "https://www.youtube.com/playlist?list=PLWPirh4EWFpFfTUVBl5KbeXly1sHKSc-Z";
-                i.putExtra("URL", s);
-                startActivity(i);
-                break;
-            case R.id.LST:
-                s="https://www.youtube.com/playlist?list=PLBlnK6fEyqRjMH3mWf6kwqiTbT798eAOm";
-                i.putExtra("URL",s);
-                startActivity(i);
-            case R.id.DM:
-                s="https://www.youtube.com/playlist?list=PLBlnK6fEyqRhqJPDXcvYlLfXPh37L89g3";
-                i.putExtra("URL",s);
-                startActivity(i);
-            case R.id.ALC:
-                s = "https://www.youtube.com/playlist?list=PLBlnK6fEyqRgp46KUv4ZY69yXmpwKOIev";
-                i.putExtra("URL", s);
-                startActivity(i);
-                break;
-            case R.id.OS:
-                s="https://www.youtube.com/playlist?list=PLBlnK6fEyqRiVhbXDGLXDk_OQAeuVcp2O";
-                i.putExtra("URL",s);
-                startActivity(i);
-            case R.id.CN:
-                s="https://www.youtube.com/playlist?list=PLBlnK6fEyqRgMCUAG0XRw78UA8qnv6jEx";
-                i.putExtra("URL",s);
-                startActivity(i);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("C PROGRAMMING");
+        arrayList.add("C++ PROGRAMMING");
+        arrayList.add("DATA STRUCTURES");
+        arrayList.add("JAVA");
+        arrayList.add("LOGIC AND SWITCHING THEORY");
+        arrayList.add("DISCRETE MATHEMATICS");
+        arrayList.add("AUTOMATA LANGUAGES AND COMPUTATION");
+        arrayList.add("OPERATING SYSTEMS");
+        arrayList.add("COMPUTER NETWORKS");
+        arrayList.add("PYTHON");
+        arrayList.add("WEB PROGRAMMING");
+        arrayList.add("ML TUTORIALS");
+        ListView listView = findViewById(R.id.mylist);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                            @Override
+                                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                                String identity = arrayAdapter.getItem(position);
+                                                Intent i;
+                                                i = new Intent(youtube.this, WebActivity.class);
+                                                String s;
+                                                switch (identity) {
+                                                    case "C PROGRAMMING":
+                                                        s = "https://www.youtube.com/playlist?list=PLBlnK6fEyqRggZZgYpPMUxdY1CYkZtARR";
+                                                        i.putExtra("URL", s);
+                                                        startActivity(i);
+                                                        break;
+                                                    case "C++ PROGRAMMING":
+                                                        s = "https://www.youtube.com/playlist?list=PLfVsf4Bjg79Cu5MYkyJ-u4SyQmMhFeC1C";
+                                                        i.putExtra("URL", s);
+                                                        startActivity(i);
+                                                        break;
+                                                    case "DATA STRUCTURES":
+                                                        s = "https://www.youtube.com/playlist?list=PL2_aWCzGMAwI3W_JlcBbtYTwiQSsOTa6P";
+                                                        i.putExtra("URL", s);
+                                                        startActivity(i);
+                                                        break;
+                                                    case "JAVA":
+                                                        s = "https://www.youtube.com/playlist?list=PLWPirh4EWFpFfTUVBl5KbeXly1sHKSc-Z";
+                                                        i.putExtra("URL", s);
+                                                        startActivity(i);
+                                                        break;
+                                                    case "LOGIC AND SWITCHING THEORY":
+                                                        s = "https://www.youtube.com/playlist?list=PLBlnK6fEyqRjMH3mWf6kwqiTbT798eAOm";
+                                                        i.putExtra("URL", s);
+                                                        startActivity(i);
+                                                        break;
+                                                    case "DISCRETE MATHEMATICS":
+                                                        s = "https://www.youtube.com/playlist?list=PLBlnK6fEyqRhqJPDXcvYlLfXPh37L89g3";
+                                                        i.putExtra("URL", s);
+                                                        startActivity(i);
+                                                        break;
+                                                    case "AUTOMATA LANGUAGES AND COMPUTATION":
+                                                        s = "https://www.youtube.com/playlist?list=PLBlnK6fEyqRgp46KUv4ZY69yXmpwKOIev";
+                                                        i.putExtra("URL", s);
+                                                        startActivity(i);
+                                                        break;
+                                                    case "OPERATING SYSTEMS":
+                                                        s = "https://www.youtube.com/playlist?list=PLBlnK6fEyqRiVhbXDGLXDk_OQAeuVcp2O";
+                                                        i.putExtra("URL", s);
+                                                        startActivity(i);
+                                                        break;
+                                                    case "COMPUTER NETWORKS":
+                                                        s = "https://www.youtube.com/playlist?list=PLBlnK6fEyqRgMCUAG0XRw78UA8qnv6jEx";
+                                                        i.putExtra("URL", s);
+                                                        startActivity(i);
+                                                        break;
+                                                    case "PYTHON":
+                                                        s = "https://www.youtube.com/playlist?list=PLsyeobzWxl7poL9JTVyndKe62ieoN-MZ3";
+                                                        i.putExtra("URL", s);
+                                                        startActivity(i);
+                                                        break;
+                                                    case "WEB PROGRAMMING":
+                                                        s = "https://www.youtube.com/playlist?list=PLwoh6bBAszPrES-EOajos_E9gvRbL27wz";
+                                                        i.putExtra("URL", s);
+                                                        startActivity(i);
+                                                        break;
+                                                    case "ML TUTORIALS":
+                                                        s = "https://www.youtube.com/playlist?list=PLQVvvaa0QuDfKTOs3Keq_kaG2P55YRn5v";
+                                                        i.putExtra("URL", s);
+                                                        startActivity(i);
+                                                        break;
 
-        }
+                                                }
+                                            }
+
+
+                                        }
+        );
     }
 }
