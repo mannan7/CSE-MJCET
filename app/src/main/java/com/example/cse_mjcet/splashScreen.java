3a59ecdbe76b982caf901976a6df6a0a79f8b28b
salman-am <salman102000@gmail.com>
2020-01-23 23:15:20 +0530

    commit

--- app/src/main/java/com/example/cse_mjcet/splashScreen.java
+++ app/src/main/java/com/example/cse_mjcet/splashScreen.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class welcome extends AppCompatActivity {
+public class splashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
